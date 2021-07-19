77f79aa982547f24cf64ca1c6b3418060214094b
Zulfadhli Mohd Zaki <zulfadhli.zaki@beicip.asia>
2021-07-19 12:34:56 +0800

    add casting demo

---
+++ src/c2/example/03_Casting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+internal class Casting
+{
+  private void Main()
+  {
+    int i = 3, j = 2;
+    float fraction;
+    fraction = i / (float)j;
+    Console.WriteLine("fraction: " + fraction);
+  }
+}
